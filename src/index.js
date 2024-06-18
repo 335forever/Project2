646db3f26025bf777757b35c57d399b6c40807f2
Nguyễn Đức Quân <87223367+335forever@users.noreply.github.com>
2024-06-19 01:42:02 +0700

    fix loi

--- src/index.js
+++ src/index.js
@@ -12,11 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(cors({   
-  origin: '*',   
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],   
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
   next();
